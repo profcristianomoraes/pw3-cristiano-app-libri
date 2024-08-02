1d05f1ae8cf234ea2cf9e035dd98bb2f01f136c8
profcristianomoraes <cristianocorrea3@gmail.com>
2024-08-01 22:21:41 -0300

    Limpeza dos arquivos CSS e criação do primeiro componente

--- pw3-cristiano-app-libri/src/App.jsx
+++ pw3-cristiano-app-libri/src/App.jsx
@@ -1,33 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
+import CardBooks from './components/CardBooks'
+
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
+      <CardBooks 
+        titulo='As Cavernas de Aço'
+        autor='Isaac Asimov'
+      />
+
+      <CardBooks 
+        titulo='O Sol Desvelado'
+        autor='Isaac Asimov'
+      />
+
+    <CardBooks 
+        titulo='O Senho dos Áneis: A Sociedade do Anel'
+        autor='J.R.R Tolkien'
+      />
+      
+
+      
     </>
   )
 }
---
+++ pw3-cristiano-app-libri/src/components/CardBooks.jsx
@@ -0,0 +1,12 @@
+const CardBooks  = ({titulo, autor})=>{
+
+    return(
+        <div>
+            <h3>{titulo}</h3>
+            <p>{autor}</p>
+        </div>
+    )
+
+}
+
+export default CardBooks
