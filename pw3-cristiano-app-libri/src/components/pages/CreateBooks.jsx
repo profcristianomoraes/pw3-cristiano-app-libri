cd5dfb56229add252beb5a775c5fa0924f6e8482
profcristianomoraes <cristianocorrea3@gmail.com>
2024-10-03 22:22:44 -0300

    Inserção de dados parte 1-1

--- pw3-cristiano-app-libri/src/components/pages/CreateBooks.jsx
+++ pw3-cristiano-app-libri/src/components/pages/CreateBooks.jsx
@@ -37,7 +37,7 @@ const CreateBooks = ()=>{
             
         ).then(
             (data)=>{
-                console.log('DATA: ' + data.data[0].nome_categoria)
+                console.log('DATA: ' + data.data[1].nome_categoria)
                 setCategorias(data.data)
             }
         ).catch(
